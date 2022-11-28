c694117ca1efbf6f37f5a94587d0058c8d6c192b
S5619397 <118506564+S5619397@users.noreply.github.com>
2022-11-28 08:27:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
+
 #include <iostream>
+
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0 , a=1 , b=1 , c=0, s=0;
+    cin >> n;
+    cout << a << endl << b << endl;
+    while (1){
+        c = a + b;
+        a = b;
+        b = c;
+    
+        s=c;
+        if (s<=n)
+        {
+        cout << c << endl;
+            
+        }
+        else
+        {
+            break;
+            
+        }
+        
+    
+    }
+    return 0;
 }
